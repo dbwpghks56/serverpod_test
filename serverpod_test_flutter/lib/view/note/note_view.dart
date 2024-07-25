e0af2ea274b6d1ac1b6a9bf68d696fd331ce40c7
wpghks <wpghks56@naver.com>
2024-07-25 11:47:08 +0900

    [Feature] 폴더 구조 변경 및 dialog 생성

--- serverpod_test_flutter/lib/view/note/note_view.dart
+++ serverpod_test_flutter/lib/view/note/note_view.dart
@@ -1,37 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:serverpod_test_flutter/repository/note_repository.dart';
-import 'package:serverpod_test_flutter/service/note_service.dart';
+import 'package:serverpod_test_flutter/view/note/widget/note_dialog.dart';
+import 'package:serverpod_test_flutter/view/note/widget/note_list.dart';
 
 class NoteView extends HookConsumerWidget {
   const NoteView({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final notes = ref.watch(noteServiceProvider);
-
-    useEffect(() {
-      ref.watch(getAllNoteRepositoryProvider);
-      return null;
-    }, []);
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Notes'),
       ),
-      body: ListView.builder(
-        itemCount: notes.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(notes[index].title),
-            subtitle: Text(notes[index].content),
-          );
-        },
-      ),
+      body: const NoteList(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          // Create a new note
+          showDialog(
+            context: context,
+            builder: (context) {
+              return const NoteDialog();
+            },
+          );
         },
         tooltip: 'Create new note',
         child: const Icon(Icons.add),
---
+++ serverpod_test_flutter/lib/view/note/widget/note_list.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:serverpod_test_flutter/repository/note_repository.dart';
+import 'package:serverpod_test_flutter/service/note_service.dart';
+
+class NoteList extends HookConsumerWidget {
+  const NoteList({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final notes = ref.watch(noteServiceProvider);
+
+    useEffect(() {
+      ref.watch(getAllNoteRepositoryProvider);
+      return null;
+    }, []);
+
+    return ListView.builder(
+      itemCount: notes.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(notes[index].title),
+          subtitle: Text(notes[index].content),
+        );
+      },
+    );
+  }
+}
