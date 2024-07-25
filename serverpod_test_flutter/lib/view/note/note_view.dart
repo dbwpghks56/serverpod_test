cfd7a9677d45f165b57602b37d218382bac065bd
wpghks <wpghks56@naver.com>
2024-07-25 11:33:55 +0900

    [Chore] 데이터 표시 할 때 변수 변경

--- serverpod_test_flutter/lib/view/note/note_view.dart
+++ serverpod_test_flutter/lib/view/note/note_view.dart
@@ -1,38 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:serverpod_test_flutter/repository/note_repository.dart';
+import 'package:serverpod_test_flutter/service/note_service.dart';
 
 class NoteView extends HookConsumerWidget {
   const NoteView({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final notes = ref.watch(getAllNoteRepositoryProvider);
+    final notes = ref.watch(noteServiceProvider);
+
+    useEffect(() {
+      ref.watch(getAllNoteRepositoryProvider);
+      return null;
+    }, []);
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Notes'),
       ),
-      body: notes.when(
-        data: (data) {
-          return ListView.builder(
-            itemCount: data.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(data[index].title),
-                subtitle: Text(data[index].content),
-              );
-            },
-          );
-        },
-        error: (error, stackTrace) {
-          return Center(
-            child: Text('Error: $error'),
-          );
-        },
-        loading: () {
-          return const Center(
-            child: CircularProgressIndicator(),
+      body: ListView.builder(
+        itemCount: notes.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(notes[index].title),
+            subtitle: Text(notes[index].content),
           );
         },
       ),
